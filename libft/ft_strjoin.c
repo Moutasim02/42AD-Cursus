e90ff7cda1781db2929624d5396c74214f9d4b7d
Moutasim02 <moutasim9997@gmail.com>
2023-07-13 19:33:05 +0400

    Split Function Start

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,34 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-ayou <mel-ayou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 16:47:11 by mel-ayou          #+#    #+#             */
+/*   Updated: 2023/07/13 17:06:28 by mel-ayou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
 	char* str;
-	size_t len1 = ft_strlen(s1);
-	size_t len2 = ft_strlen(s2);
+	size_t len1;
+	size_t len2;
 	int i;
 	int j;
-
+		
+	if(!s1 || !s2)
+		return NULL;
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 
 	i = 0;
 	while (s1[i])
-		str[i++] = s1[i];
-	
+	{
+		str[i] = s1[i];
+		i++;
+	}
 	j = 0;
 	while (s2[j])
-		str[i + j++] = s2[j]; 
+	{
+		str[i + j] = s2[j];
+		j++; 
+	}
 	
 	str[i + j] = '\0';
 	return str;
 }
-
-// int main()
-// {
-// 	char* s1 = "Hi ";
-// 	char* s2 = "Moutasim!";
-// 	printf("s1 + s2: %s", ft_strjoin(s1, s2));
-// }
